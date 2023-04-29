497c76fd6ae3aecc45d915608f1d673cbc21e005
Ömer Yazıcı <103966057+omeryaz@users.noreply.github.com>
2023-04-29 18:51:54 +0300

    Yorumlar eklendi

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+// Datalar axios yardımı ve async bir fonksiyon ile "userData" ve "userPosts" adlı iki ayrı variable'a atandı.
 const getData = async function (userId) {
   const { data: userData } = await axios(
     "https://jsonplaceholder.typicode.com/users/" + userId
@@ -8,7 +8,9 @@ const getData = async function (userId) {
     "https://jsonplaceholder.typicode.com/posts?userId=" + userId
   );
 
+  // Ödevde tek bir obje istendiği için postları içeren userPosts'u userData objesinin "posts" property'sine atandı.
   userData.posts = userPosts;
+  // console.log yardımı ile istenilen çıktı alındı.
   console.log(userData);
   return userData;
 };
